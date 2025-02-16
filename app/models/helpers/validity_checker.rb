45dd556cee9f847272590b75faa52e5ca3e7e878
Chris <chrism465@gmail.com>
2025-02-16 22:04:00 +0000

    extra check in run

--- app/models/helpers/validity_checker.rb
+++ app/models/helpers/validity_checker.rb
@@ -10,6 +10,7 @@ module ValidityChecker
   def valid_run?
     return false unless valid_number_of_tiles?
     return false unless tiles.pluck(:colour).uniq.size == 1
+    return false unless tiles.pluck(:number).sort == tiles.pluck(:number)
     return false unless tiles.pluck(:number).sort.each_cons(2).all? { |x, y| y == x + 1 }
 
     true
