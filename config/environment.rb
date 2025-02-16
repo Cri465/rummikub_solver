1f907a38cfeaf265c14570ac74619169bb3be0c1
Chris <chrism465@gmail.com>
2025-02-16 22:46:20 +0000

    standalone_migrations for rake tasks

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require
 require 'yaml'
 require 'active_record'
 
-db_config = YAML.load_file(File.expand_path('database.yml', __dir__), aliases: true)
+db_config = YAML.load_file(File.expand_path('./db/config.yml', __dir__), aliases: true)
 ENV['RACK_ENV'] ||= 'development'
 ActiveRecord::Base.establish_connection(db_config[ENV['RACK_ENV']])
 ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV['RACK_ENV'] == 'development'
